5582d41515d4aa2c3c4dd59a1696ccb1b2625286
Jpan0912 <74218391+Jpan0912@users.noreply.github.com>
2025-07-02 02:43:54 +1200

    adding nickname flow to Create Account Page

---
+++ src/components/NicknameInput.jsx
@@ -0,0 +1,15 @@
+function NicknameInput({ nickname, setNickname, error, showError }) {
+    return (
+      <div>
+        <label>Account Nickname</label>
+        <input
+          type="text"
+          value={nickname}
+          onChange={(e) => setNickname(e.target.value)}
+        />
+        {showError && error && <span className="error">{error}</span>}
+      </div>
+    );
+  }
+  
+  export default NicknameInput;
--- src/pages/CreateAccountPage.jsx
+++ src/pages/CreateAccountPage.jsx
@@ -1,11 +1,48 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import NicknameInput from '../components/NicknameInput';
 
 function CreateAccountPage() {
+    const [nickname, setNickname] = useState("");
+    const [errors, setErrors] = useState({});
+    const [showErrors, setShowErrors] = useState(false);
+
+    const handleSubmit = () => {
+        const newErrors = {};
+        if (nickname.length < 5 || nickname.length > 30) {
+          newErrors.nickname = "Nickname must be between 5 and 30 characters";
+        }
+    
+        setErrors(newErrors);
+        setShowErrors(true);
+    
+        if (Object.keys(newErrors).length === 0){
+            console.log("Form submitted:", { nickname });
+            // TODO: AddAPI request here
+        }
+      };
+
+      useEffect(() => {
+          if(showErrors && errors.nickname) {
+              if(nickname.length >= 5 && nickname.length <= 30) {
+                  setErrors(prev => ({ ...prev, nickname: null }));
+              }
+          }
+      }, [nickname]);
+
     return (
         <div>
-            <h1>Open a bank account</h1>
+            <h2>Open a bank account</h2>
+            <NicknameInput
+                nickname={nickname}
+                setNickname={setNickname}
+                error={errors.nickname}
+                showError={showErrors}
+            />
+            <button onClick={handleSubmit}>Open Bank Account</button>
         </div>
     );
 }
+
+
 
 export default CreateAccountPage;
